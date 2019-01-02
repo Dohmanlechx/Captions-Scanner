2f43c65495d67b3f458b301b4cb924537a3a97de
unknown <dohman_92@hotmail.com>
2019-01-02 16:58:31 +0100

    Game properly fetches new word after right answer

--- app/src/main/java/com/dohman/captionsscanner/GameActivity.java
+++ app/src/main/java/com/dohman/captionsscanner/GameActivity.java
@@ -1,6 +1,7 @@
 package com.dohman.captionsscanner;
 
 import android.database.Cursor;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,9 +17,15 @@ import com.dohman.captionsscanner.controller.DatabaseHelper;
 public class GameActivity extends AppCompatActivity {
     private static final String TAG = "GameActivity";
     private final DatabaseHelper db = new DatabaseHelper(this);
+    private static final Handler handler = new Handler();
+    private Cursor currentWord;
 
     private TextView answerTv;
-    private TextView textView;
+    private TextView titleTv;
+
+    private EditText editText;
+
+    int defaultColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,41 +33,53 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         answerTv = findViewById(R.id.answer);
-        textView = findViewById(R.id.tv_title);
-        final Cursor currentWord;
+        titleTv = findViewById(R.id.tv_title);
+        editText = findViewById(R.id.editText);
+
+        defaultColor = titleTv.getCurrentTextColor();
 
         if (SettingsActivity.getDefaults("databaseNotEmpty", this) == null) {
             Toast.makeText(this, getString(R.string.toastnowords), Toast.LENGTH_LONG).show();
         } else {
-            currentWord = getRandomWord();
-            textView.setText(currentWord.getString(1));
+            reset();
 
-            final EditText editText = findViewById(R.id.editText);
             editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
                 @Override
                 public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                    boolean handled = false;
                     if (actionId == EditorInfo.IME_ACTION_DONE) {
                         if (editText.getText().toString().toLowerCase().contains(currentWord.getString(2))) {
                             // CORRECT ANSWER
                             editText.setTextColor(getResources().getColor(R.color.correct_color));
-                            handled = true;
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    reset();
+                                }
+                            }, 1000);
                         } else {
                             // INCORRECT ANSWER
                             editText.setTextColor(getResources().getColor(R.color.incorrect_color));
                             answerTv.setText(currentWord.getString(2));
                             answerTv.setVisibility(View.VISIBLE);
-                            handled = true;
                         }
                     }
 
-                    return handled;
+                    return true;
                 }
             });
         }
+
     }
 
     private Cursor getRandomWord() {
         return db.randomData();
+    }
+
+    private void reset() {
+        currentWord = getRandomWord();
+        titleTv.setText(currentWord.getString(1));
+        editText.setTextColor(defaultColor);
+        answerTv.setVisibility(View.GONE);
+        editText.setText("");
     }
 }
